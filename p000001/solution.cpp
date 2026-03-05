674628b8e227a0086ef4d424fcf50f0dbeaa54c8
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-03-05 10:27:56 +0900

    Create solution.cpp
    # P0001 - Subtract Two Integers
    
    ### 🏷 Platform
    Programmers
    
    ### 🔗 Problem URL
    https://school.programmers.co.kr/learn/courses/30/lessons/120803
    
    ### 💡 Approach
    - Simple subtraction using arithmetic operator (-)
    - OOP

---
+++ p000001/solution.cpp
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+class MyCalculator {
+public:
+	int Subtract(int num1, int num2) const {
+		return num1, num2;
+	}
+};
+
+int solution(int num1, int num2) {
+	MyCalculator calc;
+	return calc.Subtract(num1, num2);
+}
