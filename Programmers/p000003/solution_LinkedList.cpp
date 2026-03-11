f02951de9b133cf0cfd75517b0c42db715388fe3
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-03-11 19:14:00 +0900

    Use Linked List

---
+++ Programmers/p000003/solution_LinkedList.cpp
@@ -0,0 +1,55 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+// Double each element of the array
+// Use Linked List
+
+#include <iostream>
+#include <sstream>
+
+struct Node {
+	int data;
+	Node* next;
+
+	Node(int val) {
+		data = val;
+		next = nullptr;
+	}
+};
+
+int main() {
+    std::string str;
+    std::getline(std::cin, str);
+
+    std::stringstream ss(str);
+
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+    int x;
+
+    while (ss >> x) {
+        Node* newNode = new Node(x);
+
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    Node* cur = head;
+    while (cur != nullptr) {
+        cur->data *= 2;
+        cur = cur->next;
+    }
+
+    cur = head;
+    while (cur != nullptr) {
+        std::cout << cur->data << " ";
+        cur = cur->next;
+    }
+
+    return 0;
+}
