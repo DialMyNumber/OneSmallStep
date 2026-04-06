f539a4b7072c041820afc94b29c6d18ee4333490
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-06 14:10:38 +0900

    Queue를 이용하지 않아도 되기 때문에 리팩토링

--- Baek/BeakLevel1/2588/2588.cpp
+++ Baek/BeakLevel1/2588/2588.cpp
@@ -1,34 +1,23 @@
 // https://www.acmicpc.net/problem/2588
 
 #include <iostream>
-#include <queue>
 
 int main(void) {
 	size_t a;
 	size_t b;
-
 	std::cin >> a;
 	std::cin >> b;
 
-	std::queue<size_t> bElement;
-	int carry = 10;
-	size_t tempB = b;
-
-	while (b > carry/10) {
-		bElement.push(tempB % 10); 
-		tempB /= 10;
-		carry *= 10;
-	}
-
 	size_t sum = 0;
-	size_t sumCarry = 1;
-	// 처음엔 std::vector로 구현했으나, FIFO = Queue가 더 적절해보임
-	size_t size = bElement.size();
-	for (int i = 0; i < size; ++i) {
-		std::cout << bElement.front() * a << std::endl;
-		sum += bElement.front() * a * sumCarry;
-		sumCarry *= 10;
-		bElement.pop();
+	size_t tempB = b;
+	size_t digit = 1;
+	for (int i = 0; tempB > 0; ++i) {
+		std::cout << a * (tempB % 10) << std::endl;
+		sum += a * (tempB % 10) * digit;
+		tempB /= 10;
+		digit *= 10;
 	}
-	std::cout << sum << std::endl;
+	std::cout << sum;
+
+	return 0;
 }
