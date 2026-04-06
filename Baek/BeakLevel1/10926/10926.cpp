5c246a509fbe780c2509e0f21da18f2a4e3b3da8
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-06 10:06:23 +0900

    입력 문자열 처리

---
+++ Baek/BeakLevel1/10926/10926.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/10926
+
+#include <iostream>
+
+int main(void) {
+
+	char str[51];
+	std::cin >> str;
+	std::cout << str << "??!";
+	
+	/*
+	std::string str;
+	std::cin >> str;
+	str += "??!";
+	std::cout << str;
+	*/
+
+	return 0;
+}
