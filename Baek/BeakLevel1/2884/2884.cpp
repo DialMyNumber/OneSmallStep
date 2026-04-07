a1547909cfa1f23ea4b90f112dd608f73443f338
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-07 20:34:53 +0900

    더 나은 방식으로 리팩토링
    https://www.acmicpc.net/problem/2884

--- Baek/BeakLevel1/2884/2884.cpp
+++ Baek/BeakLevel1/2884/2884.cpp
@@ -10,24 +10,13 @@ int main(void) {
 	std::cin >> h;
 	std::cin >> m;
 
-	if (h == 0) {
-		if (m < 45) {
-			h = 23;
-			m = 60 - (45 - m);
-			std::cout << h << " " << m;
-		}
-		else if (m == 45) {
-			h + 1;
-			std::cout << h << " " << m - 45;
-		}
-		else {
-			std::cout << h << " " << m - 45;
-		}
+	int msum = h * 60 + m;
+	
+	if (msum < 45) {
+		msum += 24 * 60;
 	}
-	else {
-		int msum = h * 60 + m;
-		int nh = (msum - 45) / 60;
-		int nm = (msum - 45) % 60;
-		std::cout << nh << " " << nm;
-	}
+	msum -= 45;
+	std::cout << msum / 60 << " " << msum % 60;
+
+	return 0;
 }
