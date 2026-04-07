b2907c9aafa0ecfa353ca28fef226ed81c08547a
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-07 20:25:57 +0900

    시간, 분이 입력으로 주어지고 추가 시간이 입력으로 주어질 때 해당 시간에서 추가 시간 이후의 시간 출력

---
+++ Baek/BeakLevel1/2525/2525.cpp
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/2525
+
+#include <iostream>
+
+int main(void) {
+
+	int h;
+	int m;
+	int t;
+
+	std::cin >> h;
+	std::cin >> m;
+	std::cin >> t;
+
+	int msum = h * 60 + m + t;
+
+	if (msum >= 60*24) {
+		msum -= 60*24;
+	}
+
+	std::cout << msum / 60 << " " << msum % 60;
+
+	return 0;
+}
