6e7cb7f3f70609f40d0cccc2c232e4d62fc0eaf6
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-07 20:47:06 +0900

    주사위 눈 3개가 입력으로 주어질 때 경우에 따른 상금 규칙 처리
    https://www.acmicpc.net/problem/2480

---
+++ Baek/BeakLevel1/2480/2480.cpp
@@ -0,0 +1,38 @@
+// https://www.acmicpc.net/problem/2480
+
+#include <iostream>
+
+int main(void) {
+	int a;
+	int b;
+	int c;
+
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+
+	if (a == b && b == c) {
+		std::cout << 10000 + a * 1000;
+	}
+	else if (a == b || b == c || a == c) {
+		if (a == b || b == c) {
+			std::cout << 1000 + b * 100;
+		}
+		else {
+			std::cout << 1000 + a * 100;
+		}
+	}
+
+	int big = a;
+	if (big < b) {
+		big = b;
+	}
+
+	if (big < c) {
+		big = c;
+	}
+
+	std::cout << big * 100;
+
+	return 0;
+}
