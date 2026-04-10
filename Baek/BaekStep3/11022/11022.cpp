94592926f93eb095eff46082479dcaa608fc0731
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-10 09:58:15 +0900

    단순 계산 및 출력
    https://www.acmicpc.net/problem/11022

---
+++ Baek/BaekStep3/11022/11022.cpp
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/11022
+
+#include <iostream>
+
+int main(void) {
+	std::cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	int T;
+	std::cin >> T;
+	int a;
+	int b;
+
+	for (int i = 1; i <= T; ++i) {
+		std::cin >> a;
+		std::cin >> b;
+
+		std::cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
+	}
+}
