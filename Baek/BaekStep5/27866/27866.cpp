7f889372fdab3906d8cff0d3df58ed2406117f69
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-15 09:47:21 +0900

    char 배열을 이용한 문자열 처리
    https://www.acmicpc.net/problem/27866

---
+++ Baek/BaekStep5/27866/27866.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/27866
+
+#include <iostream>
+
+int main(void) {
+	char S[1001];
+	std::cin >> S;
+	int i;
+	std::cin >> i;
+
+	std::cout << S[i-1];
+	return 0;
+}
