582567876e8c05f392e19c63cef30746fb346c49
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-17 11:00:18 +0900

    getline과 while문 응용
    https://www.acmicpc.net/problem/11718

---
+++ Baek/BaekStep5/11718/11718.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/11718
+
+#include <iostream>
+#include <string>
+
+int main(void) {
+	std::cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	std::string input;
+	while (std::getline(std::cin, input)) {
+		std::cout << input << '\n';
+	}
+
+	return 0;
+}
