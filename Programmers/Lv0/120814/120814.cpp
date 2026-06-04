3457e95ac2b154f7a9287af9bb91581bdfd85a7e
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-04 17:19:01 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120814

---
+++ Programmers/Lv0/120814/120814.cpp
@@ -0,0 +1,20 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120814
+#include <iostream>
+
+int main(void) {
+	int answer = 0;			// 필요한 피자의 수
+
+	int n;
+	std::cin >> n;		// 문제에서 주어질 나눠먹을 사람의 수
+
+	int piece;
+
+	for (answer; answer < 100; ++answer) {
+		piece = answer * 7 / n;
+		if (piece >= 1) {
+			return answer;
+		}
+	}
+
+	return 0;
+}
