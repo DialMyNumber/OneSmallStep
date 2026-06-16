97227613f025565f72c6fcd0536998411910b8c5
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-16 12:24:33 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120821

---
+++ Programmers/Lv0/120821/120821.cpp
@@ -0,0 +1,13 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    for (int i = num_list.size() - 1; i >= 0; --i) {     // size() = 원소의 개수
+        answer.push_back(num_list[i]);
+    }
+    return answer;
+}
