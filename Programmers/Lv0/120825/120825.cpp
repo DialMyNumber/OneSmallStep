235246a32a68d954beda1350b6ffca0f58ff6cd0
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-18 10:57:11 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120825 문자열이 주어지면 각 문자를 n번씩 반복 ex) "hello", 3 -> "hhheeellllllooo"

---
+++ Programmers/Lv0/120825/120825.cpp
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120825
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    for (int j = 0; j < my_string.size(); ++j) {
+        for (int i = 0; i < n; ++i) {
+            answer += my_string[j];
+        }
+    }
+    return answer;
+}
