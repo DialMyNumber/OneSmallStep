602d4a77964407db1e690c6b0997b2400ea35fc9
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-18 12:05:49 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120826 문자열에서 특정 문자열 제거

---
+++ Programmers/Lv0/120826/120826.cpp
@@ -0,0 +1,31 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120826
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+
+    // 방법 1 : 단순 이중 for문
+    for (int i = 0; i < my_string.size(); ++i) {
+        bool isIn = false;
+        for (int j = 0; j < letter.size(); ++j) {
+            if (my_string[i] == letter[j]) {
+                isIn = true;
+            }
+        }
+
+        if (!isIn) {
+            answer += my_string[i];
+        }
+    }
+
+    // 방법 2 : Range-based for loop + STL
+    for (char c : letter) {
+        my_string.erase(remove(my_string.begin(), my_string.end(), c), my_string.end());
+    }
+
+    return answer;
+}
