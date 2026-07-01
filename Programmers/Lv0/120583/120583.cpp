40e5e6b1012b3aee46411cb840e5b5c1ba76e8ee
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-01 09:50:02 +0900

    배열 내 특정 원소의 개수

---
+++ Programmers/Lv0/120583/120583.cpp
@@ -0,0 +1,33 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120583
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+
+    // 1. 단순 반복문
+    for (int i = 0; i < array.size(); ++i) {
+        if (array[i] == n) {
+            ++answer;
+        }
+    }
+
+    answer = 0;
+
+    // 2. Range-based
+    for (int i : array) {
+        if (i == n) {
+            ++answer;
+        }
+    }
+
+    answer = 0;
+
+    // 3. STL
+    answer = count(array.begin(), array.end(), n);
+
+    return answer;
+}
