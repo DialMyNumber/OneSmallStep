28cde049f40e519fe92633fa531a73c61c49d6c3
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-07-22 23:27:19 +0900

    중복 원소가 포함된 Vector에서 가장 다양한 선택
    중복 원소가 포함된 Vector에서 가장 다양한 선택

---
+++ Programmers/Lv1/1845/1845.cpp
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+#include <vector>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    return answer;
+}
